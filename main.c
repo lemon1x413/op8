f1d5fb8fef59e3625af6051a75d2015dc4eb073a
lemon1x413 <romalemontop@gmail.com>
2024-12-20 00:40:09 +0200

    B88B8BB8

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    double real;
+    double imaginary;
+} complex;
+
+int main() {
+    complex z;
+    z.real = 1.0;
+    z.imaginary = 1.0;
+    printf("%lf + %lfi\n", z.real, z.imaginary);
+    return 0;
+
+}
